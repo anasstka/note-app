4e0b74d1d9f5a978a3ea9e036bfa73729f91b807
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2022-01-21 11:09:02 +0600

    adding sealed classes for sorting

---
+++ app/src/main/java/com/anasstka/noteapp/feature_note/domain/util/NoteOrder.kt
@@ -0,0 +1,9 @@
+package com.anasstka.noteapp.feature_note.domain.util
+
+sealed class NoteOrder(
+    val orderType: OrderType
+) {
+    class Title(orderType: OrderType): NoteOrder(orderType)
+    class Color(orderType: OrderType): NoteOrder(orderType)
+    class Date(orderType: OrderType): NoteOrder(orderType)
+}
---
+++ app/src/main/java/com/anasstka/noteapp/feature_note/domain/util/OrderType.kt
@@ -0,0 +1,6 @@
+package com.anasstka.noteapp.feature_note.domain.util
+
+sealed class OrderType {
+    object Ascending: OrderType()
+    object Descending: OrderType()
+}
