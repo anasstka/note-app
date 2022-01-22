4874eb79842bf32d4a3800fc63e17fac69f1a14d
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2022-01-22 10:47:38 +0600

    connection dagger hilt

---
+++ app/src/main/java/com/anasstka/noteapp/di/AppModule.kt
@@ -0,0 +1,47 @@
+package com.anasstka.noteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.anasstka.noteapp.feature_note.data.data_source.NoteDatabase
+import com.anasstka.noteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.anasstka.noteapp.feature_note.domain.repository.NoteRepository
+import com.anasstka.noteapp.feature_note.domain.use_case.AddNoteUseCase
+import com.anasstka.noteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.anasstka.noteapp.feature_note.domain.use_case.GetNotesUseCase
+import com.anasstka.noteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providesNoteUseCases( repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotes = GetNotesUseCase(repository),
+            deleteNote = DeleteNoteUseCase(repository),
+            addNote = AddNoteUseCase(repository)
+        )
+    }
+}
