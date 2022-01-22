bb73400f84e428c8a480d054043f03d4b10fe943
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2022-01-22 10:50:57 +0600

    creation a new use case for adding note

---
+++ app/src/main/java/com/anasstka/noteapp/feature_note/domain/use_case/AddNoteUseCase.kt
@@ -0,0 +1,23 @@
+package com.anasstka.noteapp.feature_note.domain.use_case
+
+import com.anasstka.noteapp.feature_note.domain.model.InvalidNoteException
+import com.anasstka.noteapp.feature_note.domain.model.Note
+import com.anasstka.noteapp.feature_note.domain.repository.NoteRepository
+import kotlin.jvm.Throws
+
+class AddNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note) {
+        if (note.title.isBlank()) {
+            throw InvalidNoteException("The title of the note can't be empty.")
+        }
+        if (note.content.isBlank()) {
+            throw InvalidNoteException("The content of the note can't be empty.")
+        }
+
+        repository.insertNote(note)
+    }
+}
--- app/src/main/java/com/anasstka/noteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/anasstka/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,7 @@
 package com.anasstka.noteapp.feature_note.domain.use_case
 
 data class NoteUseCases(
-    val getNotes: GatNotesUseCase,
-    val deleteNote: DeleteNoteUseCase
+    val getNotes: GetNotesUseCase,
+    val deleteNote: DeleteNoteUseCase,
+    val addNote: AddNoteUseCase
 )
