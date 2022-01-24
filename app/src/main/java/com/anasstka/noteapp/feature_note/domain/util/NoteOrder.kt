44d37468c7e4a8df90ab193b8939a09045805246
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2022-01-24 20:33:56 +0600

    adding a function copy to the class NoteOrder to change the order type

--- app/src/main/java/com/anasstka/noteapp/feature_note/domain/util/NoteOrder.kt
+++ app/src/main/java/com/anasstka/noteapp/feature_note/domain/util/NoteOrder.kt
@@ -3,7 +3,15 @@ package com.anasstka.noteapp.feature_note.domain.util
 sealed class NoteOrder(
     val orderType: OrderType
 ) {
-    class Title(orderType: OrderType): NoteOrder(orderType)
-    class Color(orderType: OrderType): NoteOrder(orderType)
-    class Date(orderType: OrderType): NoteOrder(orderType)
+    class Title(orderType: OrderType) : NoteOrder(orderType)
+    class Color(orderType: OrderType) : NoteOrder(orderType)
+    class Date(orderType: OrderType) : NoteOrder(orderType)
+
+    fun copy(orderType: OrderType): NoteOrder {
+        return when(this) {
+            is Title -> Title(orderType)
+            is Color -> Color(orderType)
+            is Date -> Date(orderType)
+        }
+    }
 }
