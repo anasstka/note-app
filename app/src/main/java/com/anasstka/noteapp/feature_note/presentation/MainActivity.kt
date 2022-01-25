5e27990f1ec22672914bb208174a8f84008c8ce6
Anastasia Tsvetkova <nastyushka20020711@gmail.com>
2022-01-25 21:16:04 +0600

    creating start activity

--- app/src/main/java/com/anasstka/noteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/anasstka/noteapp/feature_note/presentation/MainActivity.kt
@@ -3,13 +3,27 @@ package com.anasstka.noteapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.navigation.compose.rememberNavController
+import com.anasstka.noteapp.feature_note.presentation.util.AppNavigation
 import com.anasstka.noteapp.ui.theme.NoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NoteAppTheme {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    AppNavigation(navController = navController)
+                }
             }
         }
     }
